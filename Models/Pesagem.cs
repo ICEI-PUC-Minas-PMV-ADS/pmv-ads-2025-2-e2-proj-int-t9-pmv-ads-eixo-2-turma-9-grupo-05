e7e6588f0d45ad743c9babd6627b3c999a022302
diogojoia777 <diogojoia.zin@gmail.com>
2025-10-25 21:16:15 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Pesagem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+[Table("Pesagem")]
+
+public class Pesagem
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Obrigatório informar o tipo de resíduo.")]
+    public string Tipo { get; set; }
+
+    [Required(ErrorMessage = "Obrigatório informar o peso do resíduo.")]
+    public double Peso { get; set; }
+
+    [Required]
+    public DateTime DataHora { get; set; }
+
+    [ForeignKey("Usuario")]
+    public int UsuarioId { get; set; }
+
+    [InverseProperty("Pesagens")]
+    public Usuario Usuario { get; set; }
+
+    [NotMapped]
+    public Pesagem()
+    {
+        DataHora = DateTime.Now;
+    }
+
+}
