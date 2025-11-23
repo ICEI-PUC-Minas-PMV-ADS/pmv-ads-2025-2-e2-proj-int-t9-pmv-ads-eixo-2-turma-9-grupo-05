3af2d3554bf69090f160e4a521c6cb1d078ca3b6
GabrielR3zende <80123472+GabrielR3zende@users.noreply.github.com>
2025-11-23 13:09:58 -0300

    Add files via upload

---
+++ src/main/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using EcoTrash.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoTrash.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        
+        public DbSet<Empresa> Empresas { get; set; } = null!;
+        public DbSet<Pesagem> Pesagens { get; set; } = null!;
+        public DbSet<LocalColeta> Locais { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+            modelBuilder.Entity<Empresa>()
+                .HasIndex(e => e.CNPJ)
+                .IsUnique();
+
+            modelBuilder.Entity<Empresa>()
+                .HasIndex(e => e.Email)
+                .IsUnique();
+
+            
+            modelBuilder.Entity<Pesagem>().ToTable("Pesagens");
+            modelBuilder.Entity<LocalColeta>().ToTable("Locais");
+        }
+    }
+}
